68c28bc6e5fe06c21deb63a5fc297de8f5219f4f
Mustafa1909 <yousufmusf@gmail.com>
2026-06-13 11:10:45 +0530

    Rotate array by K

--- reverse.java
+++ reverse.java
@@ -1,6 +1,9 @@
 public class reverse {
     public static void main(String[] args) {
+        
         int arr[]={1,2,3,4,5,6,7,8};
+        int n = arr.length;
+        
         int k = 2;
         swap(arr,2);
         
---
+++ reversebyk.java
@@ -0,0 +1,34 @@
+public class reversebyk {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        reverse(arr,2);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+        
+      
+        
+    }
+    public static void reverse(int arr[],int k){
+        int n=arr.length;
+        k=k%n;
+
+        reversebyk(arr,0,k-1);
+        reversebyk(arr,k,n-1);
+        reversebyk(arr,0,n-1);
+
+    }
+    public static void reversebyk(int arr[],int s,int e){
+        
+       
+        while(s<e){
+            int temp = arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+        }
+       
+    }
+}
