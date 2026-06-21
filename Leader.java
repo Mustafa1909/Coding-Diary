50884a87b8438580da30ddb10fc69adbe76fb5df
Mustafa1909 <yousufmusf@gmail.com>
2026-06-21 11:47:49 +0530

    Leader Array

--- Leader.java
+++ Leader.java
@@ -1,16 +1,28 @@
-import java.util.ArrayList;
+import java.util.*;
 public class Leader {
-    public static void main(String[] args) {
-        int arr[]= {16, 17, 4, 3, 5, 2};
+   public static void main(String[] args) {
       
-        leader(arr);
-}
-public static void leader(int arr[]){
-   
-   for(int i=0;i<arr.length-1;i++){
-    if(arr[i]>arr[i+1]){
-       System.out.println(arr[i]);
-    }
+       int arr[] = {16, 17, 4, 3, 5, 2};
+       findmax(arr);
    }
-}
+   public static void findmax(int arr[]){
+      ArrayList<Integer> list = new ArrayList<>();
+
+      int max = arr[arr.length-1];
+      list.add(max);
+
+      for(int i=arr.length-2;i>=0;i--){
+         if(arr[i]>max){
+            list.add(arr[i]);
+            max=arr[i];
+         }
+      }
+      for(int i=list.size()-1;i>=0;i--){
+         System.out.print(list.get(i)+" ");
+
+      }
+   }
+   
+
+   
 }
