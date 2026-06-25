e5a40f0fd78b73997d2f86e91cec5b78ea3485fe
Mustafa1909 <yousufmusf@gmail.com>
2026-06-25 06:06:28 +0530

    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome{
+    public static void main(String[] args) {
+        String str ="abba";
+         System.out.println(Check(str));
+
+
+        
+        }
+        public static boolean Check(String str){
+            int s = 0;
+            int e = str.length()-1;
+            while(s<e){
+                if(str.charAt(s)!=str.charAt(e)){
+                    return false;
+                }
+                s++;
+                e--;
+            }
+            return true;
+        }
+    }
