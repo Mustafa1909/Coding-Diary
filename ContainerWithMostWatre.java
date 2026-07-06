464f0a25992eaa5ee240f8a548008272b20c2d7e
Mustafa1909 <yousufmusf@gmail.com>
2026-07-06 07:19:17 +0530

    Container With Most Water

---
+++ ContainerWithMostWatre.java
@@ -0,0 +1,28 @@
+public class ContainerWithMostWatre {
+    public static void main(String[] args) {
+        int nums[]={1,8,6,2,5,4,8,3,7};
+       int res = MaxHeight(nums);
+       System.out.println(res);
+    }
+    public static int MaxHeight(int nums[]){
+        int left =0;
+        int right = nums.length-1;
+        int MaxArea = 0;
+
+        while(left<right){
+            int height = Math.min(nums[left],nums[right]);
+            int width = right-left;
+            int area = height*width;
+            MaxArea=Math.max(MaxArea, area);
+            if(nums[left]<nums[right]){
+                left++;
+            }else{
+                right--;
+            }
+
+
+
+        }
+        return MaxArea;
+    }
+}
