5d80ebb29bb4c24c7d595de78f3d7555383e592e
Mustafa1909 <yousufmusf@gmail.com>
2026-07-09 22:38:20 +0530

    PreOrder Traversel

---
+++ BinaryTree.java
@@ -0,0 +1,42 @@
+public class BinaryTree{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null; 
+        }
+    }
+    static class BuildTree{
+        static int idx =-1;
+        public static Node build(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left=build(nodes);
+            newNode.right=build(nodes);
+            return newNode;
+        }
+        public static void preOrder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+    public static void main(String[] args) {
+
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BuildTree bt = new BuildTree();
+       Node root = bt.build(nodes);
+       bt.preOrder(root);    
+    }
+}
