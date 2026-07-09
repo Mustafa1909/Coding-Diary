44ae3dfba2afa7dc0f2c664edaeeb540f4cafe51
Mustafa1909 <yousufmusf@gmail.com>
2026-07-09 22:47:33 +0530

    LevelOrder

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree{
     static class Node{
         int data;
@@ -31,12 +34,42 @@ public class BinaryTree{
             preOrder(root.left);
             preOrder(root.right);
         }
+        public static void levelOrder(Node root){
+            if(root == null){
+                return ;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode == null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                
+                }else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left != null ){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right !=null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+
+        }
     }
     public static void main(String[] args) {
 
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BuildTree bt = new BuildTree();
        Node root = bt.build(nodes);
-       bt.preOrder(root);    
+       bt.levelOrder(root);    
     }
 }
