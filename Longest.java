118ca8efc362c0822b968a3ad2ada7e06669d373
Mustafa1909 <yousufmusf@gmail.com>
2026-07-29 06:45:45 +0530

    Longest common prefix

--- Longest.java
+++ Longest.java
@@ -2,18 +2,30 @@ public class Longest{
     public static void main(String[] args) {
         String[] strs = {"flower","flow","flight"};
 
-    
-        String first = strs[0];
+        System.out.println(longestSeq(strs));
 
-        for(int i = 0;i<first.length();i++){
+    
+    }
+    public static String longestSeq(String[] strs){
+        if(strs.length==0){
+            return "";
+        }
+        
+        String first=strs[0];
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<first.length();i++){
             char ch = first.charAt(i);
 
             for(int j=1;j<strs.length;j++){
-                
+                if(ch!=strs[j].charAt(i)){
 
-
+                    return sb.toString();
+                }
             }
+            sb.append(ch);
         }
+        return sb.toString();
 
     }
     
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,1 @@
-root
+strs
